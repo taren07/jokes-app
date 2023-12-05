51e634658355a26a4770eb6fa70ae93ecd98c997
taren07 <taro.enchi@gmail.com>
2023-12-05 23:27:34 +0900

    add style files

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,23 @@
+import type { LinksFunction } from "@remix-run/node";
 import { Links, LiveReload, Outlet } from "@remix-run/react";
+
+import globalLargeStylesUrl from "~/styles/global-large.css";
+import globalMediumStylesUrl from "~/styles/global-medium.css";
+import globalStylesUrl from "~/styles/global.css";
+
+export const links: LinksFunction = () => [
+	{ rel: "stylesheet", href: globalStylesUrl },
+	{
+		rel: "stylesheet",
+		href: globalMediumStylesUrl,
+		media: "print, (min-width: 640px)",
+	},
+	{
+		rel: "stylesheet",
+		href: globalLargeStylesUrl,
+		media: "screen and (min-width: 1024px)",
+	},
+];
 
 export default function App() {
 	return (
