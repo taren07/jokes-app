83f5e1354171c661b3a2119260c61fbc2e9d37db
taren07 <taro.enchi@gmail.com>
2023-12-05 23:16:08 +0900

    add style

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,3 +1,11 @@
+import type { LinksFunction } from "@remix-run/node";
+
+import stylesUrl from "~/styles/index.css";
+
+export const links: LinksFunction = () => [
+	{ rel: "stylesheet", href: stylesUrl },
+];
+
 export default function IndexRoute() {
 	return <div>Hello Index Route</div>;
 }
