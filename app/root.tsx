6c6cfa601e1cf49d24d3fb77490362b131e80ef9
taren07 <taro.enchi@gmail.com>
2023-12-05 23:19:51 +0900

    fix styles dir

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import { LiveReload, Outlet } from "@remix-run/react";
+import { Links, LiveReload, Outlet } from "@remix-run/react";
 
 export default function App() {
 	return (
@@ -7,6 +7,7 @@ export default function App() {
 				<meta charSet="utf-8" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<title>Remix: So great, it's funny!</title>
+				<Links />
 			</head>
 			<body>
 				<Outlet />
